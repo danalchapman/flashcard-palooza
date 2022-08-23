72e1995afe5a03379c4980caa402223c87ae5570
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-23 09:40:05 -0500

    Create and start Turn and Turn-test files

---
+++ test/Turn-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe.only('Turn', function() {
+
+    it('should be a function', function() {
+        const turn = new Turn();
+        expect(Turn).to.be.a('function');
+    })
+})
