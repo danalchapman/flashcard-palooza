b70aa5c0990e22594750ff7e5597154a99e81dc3
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-23 18:03:58 -0500

    Pseudocode Round class

---
+++ src/Round.js
@@ -0,0 +1,13 @@
+// Round class should have these methods:
+// returnCurrentCard: returns current card being played
+// takeTurn: method that updates Turn count, evaluates guesses, gives feedback, stores ids of incorrect guesses
+// --When a guess is made, new Turn instance created
+// --Turn count updated (regardless of correct/incorrect)
+// --next card becomes currentCard
+// --guess is evaluated/recorded (incorrect guesses will be stored via the id) in an array called incorrectGuesses
+// --feedback is returned (giveFeedback)
+// calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
+// endRound: method that prints "Round over! You answered <>% of the questions correctly!"
+
+
+// module.exports = Round;
