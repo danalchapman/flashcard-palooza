54eb6ca1c62bef609dcdc7f8cf332a91d5e5e527
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-23 14:40:35 -0500

    Add beforeEach to Turn-test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,12 +1,29 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe.only('Turn', function() {
+    let card, turn;
+
+    beforeEach(function () {
+        card = new Card(1, 'What is my favorite animal', ['Penguin', 'Wolf', 'Fox', 'Dog'], 'Fox');
+        turn = new Turn('Fox', card);
+    });
 
     it('should be a function', function() {
-        const turn = new Turn();
         expect(Turn).to.be.a('function');
-    })
+    });
+
+    it('should be an instance of Turn', function () {
+        expect(turn).to.be.an.instanceOf(Turn)
+    });
+
+    it('should accept two arguments', function() {
+        expect(turn.guess).to.equal('Fox');
+        expect(turn.card).to.equal(card);
+    });
+
+    
 })
